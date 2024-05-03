dfe5e48fc149c613182c5fb6967e3aa5f2ef839d
Akhil846 <akhilvemula18@gmail.com>
2024-05-03 18:57:15 +0530

    second commit

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -24,7 +24,7 @@ const Navbar = () => {
 <nav className={`navbar navbar-expand-sm fixed-top ${isscrolled ? 'bg-black':'bg-transparent'}`}>
   <div className="container">
       <a className="navbar-brand" href="#">
-        <img src='/src/assets/NLogo.png' alt="Bootstrap" width="150vh" height="60vh"/>
+        <img src='src/assets/NLogo.png' alt="Bootstrap" width="150vh" height="60vh"/>
       </a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation" onClick={handleToggle}>
       <span className="navbar-toggler-icon"></span>
